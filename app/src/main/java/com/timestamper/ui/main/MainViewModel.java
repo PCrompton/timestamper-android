2d0a04efc243892f567c8964afa0918bc83f2cd4
Paul Crompton <beethoven89.paul@gmail.com>
2019-12-27 17:32:22 -0500

    Add button action (#3)
    * Add button click handler in fragment
    
    * Add button action and connect it to ViewModel

--- app/src/main/java/com/timestamper/ui/main/MainViewModel.java
+++ app/src/main/java/com/timestamper/ui/main/MainViewModel.java
@@ -1,7 +1,16 @@
 package com.timestamper.ui.main;
 
+import android.util.Log;
+
+import java.sql.Timestamp;
+
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    public void sendTimestamp() {
+        int timestamp = (int)System.currentTimeMillis();
+        System.out.println("Timestamp: " + timestamp);
+        //TODO implement the logic to send to repository
+    }
 }
