0832829ef1e6f6268996b1fb9bd4a46644c5a043
Bryan Crompton <bryan.crompton@gmail.com>
2019-12-31 16:16:44 -0500

    Fix timestamp accuracy problem (#7) (#9)

--- app/src/main/java/com/timestamper/model/Timestamp.java
+++ app/src/main/java/com/timestamper/model/Timestamp.java
@@ -1,14 +1,9 @@
 package com.timestamper.model;
 
 public class Timestamp {
-    int unix_timestamp;
+    Long unix_timestamp;
 
     public Timestamp() {
-        unix_timestamp = (int)System.currentTimeMillis();
-    }
-
-    public static void main(String[] args) {
-        Timestamp myObj = new Timestamp();
-        System.out.println(myObj.unix_timestamp);
+        unix_timestamp = System.currentTimeMillis()/1000;
     }
 }
